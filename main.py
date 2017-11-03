3db05802754f6d65772b648ea2891c08689ead82
Seasy <seasy.huang@gmail.com>
2017-11-02 23:21:20 -0400

    filters completed, need to implement random or for loop filter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PIL import Image
+from PIL import ImageFilter
 import glob, os
 import shutil
 
@@ -14,6 +15,89 @@ def upside_down(input_image):
 def horizontal_flip(input_image):
 
     output_image = input_image.transpose(Image.FLIP_LEFT_RIGHT)
+    # output_image.show()
+    return output_image
+
+# --------------------------------------------
+
+def blurry_boi(input_image):
+
+    output_image = input_image.filter(ImageFilter.GaussianBlur(radius=10))
+    # output_image.show()
+    return output_image
+
+# --------------------------------------------
+
+def long_boi(input_image):
+    dim = get_dimensions(input_image)
+
+    width = dim[0]
+    height = dim[1] * 1.5
+
+    size = int(width), int(height)
+
+    output_image = input_image.resize(size)
+
+    left = 0
+    upper = dim[1]/4
+    right = width
+    lower = dim[1] + dim[1]/4
+
+    box = (left, upper, right, lower)
+
+    output_image = output_image.crop(box)
+
+    # output_image = output_image.resize(dim)
+    # output_image.show()
+    return output_image
+
+# --------------------------------------------
+
+def fat_boi(input_image):
+    dim = get_dimensions(input_image)
+
+    width = dim[0] * 1.5
+    height = dim[1]
+
+    size = int(width), int(height)
+
+    output_image = input_image.resize(size)
+
+    left = dim[0]/4
+    upper = 0
+    right = dim[0] + dim[0]/4
+    lower = height
+
+    box = (left, upper, right, lower)
+
+    output_image = output_image.crop(box)
+
+    # output_image = output_image.resize(dim)
+    # output_image.show()
+    return output_image
+
+# --------------------------------------------
+
+def zoom_boi(input_image):
+
+    dim = get_dimensions(input_image)
+
+    left = dim[0]/2 - dim[0]/4
+    upper = dim[1]/2 - dim[1]/4
+    right = dim[0]/2 + dim[0]/4
+    lower = dim[1]/2 + dim[1]/4
+
+    box = (left, upper, right, lower)
+    output_image = input_image.crop(box)
+    output_image = output_image.resize(dim)
+    # output_image.show()
+    return output_image
+
+# --------------------------------------------
+
+def quantize(input_image):
+
+    output_image = input_image.quantize(colors=10, method=None, kmeans=0, palette=None)
     # output_image.show()
     return output_image
 
@@ -54,8 +138,8 @@ def overlay(input_image):
 
 # ------------------ main ------------------
 
-filename = raw_input("Enter the image path (OR type 'rm' to remove output_img folder): ")
-
+# filename = raw_input("Enter the image path (OR type 'rm' to remove output_img folder): ")
+filename = "./test.png"
 # lazy remove output
 if (filename == 'rm'):
     if os.path.exists('./output_imgs'):
@@ -71,14 +155,17 @@ if not os.path.exists('./output_imgs'):
 input_image = Image.open(filename)
 input_image.save('./output_imgs/original.png')
 
-to_save = overlay(input_image)
+to_save = fat_boi(input_image)
 to_save.save('./output_imgs/1.png')
 
-to_save = upside_down(input_image)
-to_save.save('./output_imgs/2.png')
-
-to_save = horizontal_flip(input_image)
-to_save.save('./output_imgs/3.png')
+# to_save = overlay(input_image)
+# to_save.save('./output_imgs/1.png')
+#
+# to_save = upside_down(input_image)
+# to_save.save('./output_imgs/2.png')
+#
+# to_save = horizontal_flip(input_image)
+# to_save.save('./output_imgs/3.png')
 
 
 input_image.close()
